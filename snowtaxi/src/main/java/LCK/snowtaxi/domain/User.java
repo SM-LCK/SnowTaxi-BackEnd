b572014aae69ef13f552cf73a3108da989692356
kelly1422 <kelly1422@naver.com>
2023-06-12 03:45:55 +0900

    Add: User 도메인에 블록체인 주소 속성 추가

--- snowtaxi/src/main/java/LCK/snowtaxi/domain/User.java
+++ snowtaxi/src/main/java/LCK/snowtaxi/domain/User.java
@@ -17,5 +17,6 @@ public class User {
     //private String name;
     private String kakaoId;
     private String phone;
+    private String walletAddress;
 
 }
--- snowtaxi/src/main/java/LCK/snowtaxi/service/SnowCashService.java
+++ snowtaxi/src/main/java/LCK/snowtaxi/service/SnowCashService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.EthAccounts;
 import org.web3j.protocol.http.HttpService;
 
 import java.io.File;
@@ -15,7 +16,7 @@ public class BlockChainService {
     // Ethereum 네트워크에 연결
     static Web3j web3j = Web3j.build(new HttpService("http://127.0.0.1:8545"));
 
-    public void getEthCreateAccount(User user) throws Exception {
+    public void createAccount(User user) throws Exception {
         try {
             // 새 계정 생성
             String password = "snowtaxi";
@@ -30,5 +31,15 @@ public class BlockChainService {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public String getAccount(int idx) throws Exception {
+        try {
+            EthAccounts accounts = web3j.ethAccounts().send();
+            return accounts.getAccounts().get(idx);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
