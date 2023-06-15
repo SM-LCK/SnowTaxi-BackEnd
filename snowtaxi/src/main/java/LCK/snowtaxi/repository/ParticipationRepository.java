e34eeb31346f443ec26e376cc97cd254c72b364f
kelly1422 <kelly1422@naver.com>
2023-06-15 20:55:21 +0900

    Add: findByUserIdAndPotlistId

--- snowtaxi/src/main/java/LCK/snowtaxi/repository/ParticipationRepository.java
+++ snowtaxi/src/main/java/LCK/snowtaxi/repository/ParticipationRepository.java
@@ -1,9 +1,13 @@
 package LCK.snowtaxi.repository;
 
 import LCK.snowtaxi.domain.Participation;
+import LCK.snowtaxi.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+    Optional<Participation> findByUserIdAndPotlistId(long userId, long potlistId);
 }
